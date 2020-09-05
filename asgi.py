6b57e17c73ec7319fa3ca6b1f0aa928393089727
tejbad <62945518+tejbad@users.noreply.github.com>
2020-09-05 14:56:58 +0530

    Initial commit

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Edustack project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edustack.settings')
+
+application = get_asgi_application()
---
+++ urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    url('registration/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
+    path('post/',include('post.urls')),
+    path('papers/',include('papers.urls')),
+
+
+    # path('seller/',include('seller.urls')),
+    # path('buyer/',include('buyer.urls')),
+    # path('cart/',include('cart.urls')),
+    path('',include('main.urls')),
+
+
+    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    # url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     auth_views.password_reset_confirm, name='password_reset_confirm'),
+    # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+]
+# urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
